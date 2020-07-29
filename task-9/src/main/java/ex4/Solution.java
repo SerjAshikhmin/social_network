4ff852dcb5ee9210abf99559f66dca5f83adf608
sergey_ashikhmin <serashihmin@yandex.ru>
2020-07-29 19:21:02 +0300

    Task-9: Fixed comments

--- task-9/src/main/java/ex4/Solution.java
+++ task-9/src/main/java/ex4/Solution.java
@@ -5,6 +5,8 @@ public class Solution {
     public static void main(String[] args) {
         ServiceThread serviceThread = new ServiceThread(1);
         Thread thread = new Thread(serviceThread);
+        thread.setDaemon(true);
         thread.start();
+        while (true) {}
     }
 }
